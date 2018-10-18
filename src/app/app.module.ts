e8345c8301eff13e3ad17841a1e8faddd8795bf8
Mihai Maxim <mihaimaxim90@icloud.com>
2018-10-18 23:24:37 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { AboutComponent } from './about/about.component';
+import { LocationComponent } from './location/location.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SidebarComponent,
+    AboutComponent,
+    LocationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
